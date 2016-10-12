385a7142bd459d0fb9de9b8726a9c3f0848e7bd7
nguyenchanh <nguyenchanh2201>
2016-10-12 23:25:24 +0700

    add audi file in wav/test/speaker_09/31-200.wav

--- create_fields.sh
+++ create_fields.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-echo "###################### Welcome to recording ########################" 
+echo "###################### Creating fields ########################" 
 echo "##			   READY 			        ##"
 echo "##################################################################"
 
 
-<<<<<<< HEAD
-for i in {2076..3000}
-=======
-for i in {3001..3149}
->>>>>>> 429d0f4b5a69b7424275f45ce006a1c3d05aa2dd
+
+for i in {31..200}
 do
    echo "train/speaker_09/$i" >> ./fields.txt
 
